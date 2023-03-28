5d7e6e8e890d181f24ea89f2dc4f4d8025769a01
france <france.2551@hotmail.com>
2023-03-28 18:32:36 +0700

    modified:   printsometing.py

--- printsometing.py
+++ printsometing.py
@@ -1,6 +1,6 @@
 
 import requests
-msg = 'Hello GIT'
+msg = 'Hello GIT from x'
 url = 'https://notify-api.line.me/api/notify'
 token ='h8MKeseoDbKeXyo0sofRbhpw6EhBhHLfXRl9pWv7XGH'
 headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
