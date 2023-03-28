3986f38494224038e8ac3c14b46f582adae225b5
france <france.2551@hotmail.com>
2023-03-28 18:31:02 +0700

    new file:   printsometing.py

---
+++ printsometing.py
@@ -0,0 +1,9 @@
+
+import requests
+msg = 'Hello GIT'
+url = 'https://notify-api.line.me/api/notify'
+token ='h8MKeseoDbKeXyo0sofRbhpw6EhBhHLfXRl9pWv7XGH'
+headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+
+r = requests.post(url, headers=headers, data = {'message':msg})
+print (r.text)
